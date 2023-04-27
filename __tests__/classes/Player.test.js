dc0818bcaa7025b2744f82eb5a774d935e7f07d7
Banks <CXB3393@yum.com>
2023-04-27 10:29:27 -0700

    update

--- __tests__/classes/Player.test.js
+++ __tests__/classes/Player.test.js
@@ -1,1 +1,23 @@
+const Player = require("../../classes/Player");
+const Item = require("../../classes/Item");
+const Player = require("../../classes/Player");
 
+describe("Player", () => {
+    it ("Should add an item to the inventory IF it is not already present", () => {
+        const player = new Player ("sword", "John", "human", "forest")
+        const item = new Item("potion", 5);
+        player.addItem(Item);
+        expect(player.inventory).toContain(item);
+    
+    });
+
+    it("Shouldn't add an item to inventory if its already present", () =>{
+        const logSpy = jest.spyOn(global.console, "log");
+        const player = new Player ("sword", "John", "human", "forest")
+        const item = new Item("potion", 5);
+        player.addItem(item);
+        expect(player.inventory).toHaveLength(1);
+        expect(logSpy).toHaveBeenCalledWith(`${item.name} already in inventory.`)
+    })
+
+});
--- classes/Player.js
+++ classes/Player.js
@@ -4,7 +4,17 @@ class Player extends Person {
     constructor(weapon, name, type, location) {
         super(name, type, location);
         this.weapon = weapon;
+        this.inventory = []
     };
+    addItem(item){
+        if (!this.inventory.includes(item)){
+        this.inventory.push(item);
+        return;
+    }
+    else{
+        console.log(`${item.name} already in inventory.`);
+    }
+    }
 };
 
-module.exports = Player;
+module.exports = Player; 
